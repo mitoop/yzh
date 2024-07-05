c3601a776fd204990f512ea5ceba658bd708e07d
mitoop <me@mitoop.com>
2024-07-05 10:17:12 +0800

    优化

--- src/Response.php
+++ src/Response.php
@@ -2,7 +2,13 @@
 
 namespace Mitoop\Yzh;
 
+use Illuminate\Support\Collection;
+use Yzh\Model\Apiusersign\ApiUserSignContractResponseData;
+use Yzh\Model\Apiusersign\ApiUserSignReleaseResponseData;
+use Yzh\Model\Apiusersign\ApiUserSignResponseData;
 use Yzh\Model\BaseResponse;
+use Yzh\Model\Payment\CreateAlipayOrderResponseData;
+use Yzh\Model\Payment\CreateBankpayOrderResponseData;
 
 class Response
 {
@@ -18,9 +24,40 @@ class Response
         return $this->status;
     }
 
-    public function data(): mixed
+    public function data(): Collection
     {
-        return $this->response?->getData();
+        if ($response = $this->response) {
+            $data = $response->getData();
+
+            if ($data instanceof ApiUserSignContractResponseData) {
+                return collect([
+                    'title' => $data->getTitle(), // 协议名称
+                    'url' => $data->getUrl(), // 预览跳转 URL
+                ]);
+            }
+
+            if ($data instanceof ApiUserSignResponseData) {
+                return collect([
+                    'status' => $data->getStatus(), // 是否签约成功
+                ]);
+            }
+
+            if ($data instanceof ApiUserSignReleaseResponseData) {
+                return collect([
+                    'status' => $data->getStatus(), // 是否解约成功
+                ]);
+            }
+
+            if ($data instanceof CreateBankpayOrderResponseData || $data instanceof CreateAlipayOrderResponseData) {
+                return collect([
+                    'order_id' => $data->getOrderId(), // 平台企业订单号
+                    'pay' => $data->getPay(), // 订单金额
+                    'ref' => $data->getRef(), // 综合服务平台流水号
+                ]);
+            }
+        }
+
+        return collect();
     }
 
     public function error(): string
